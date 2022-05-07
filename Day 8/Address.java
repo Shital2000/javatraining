af39fb74cb55d296328008f370a15aa03d3f373b
Shital2000 <sdhengle231@gmail.com>
2022-05-07 14:46:19 +0530

    inital

---
+++ Day 8/Address.java
@@ -0,0 +1,69 @@
+class Address
+{
+int housename;
+String areanum;
+String city;
+//getter and setter for vairable
+void setHousename(int x)
+{
+housename=x;
+}
+
+void int getHousename();
+{
+return x;
+}
+//set get housename
+void setAreaname(string abc)
+{
+area areaname=abc;
+}
+void int getAreaname()
+{
+
+
+}
+class Student
+{
+int i,d;
+String name;
+Address adr;
+public void setId(int Id)
+{
+this.id =id;
+ void setAdr(Address adr)
+{
+this.adr=adr;
+}
+public int getId()
+return id;
+}
+public Address getAdr()
+{
+return adr;
+}
+class Demo
+public static void main(String[]args)
+{
+Address ad=new Address();
+ad.sethousenum(1);
+ad.setAreaname("abc");
+ad.setcity("Aurngabad");
+Student st=new student();
+int x=10;
+st.setId(x);
+st.setAdr(add);
+st.setName("xyz");
+int id1;
+id1 = st.getId();
+String n;
+n= stgetNmae();
+Address ad1;
+ad1= st.getAdr();
+int hn;
+hn=ad1.getHousenum();
+String area;
+area=ad1.getHousenum();
+String area;
+area=adr.getAreaname();
+String city=ad1.getcity();
