80fd8d13c1c8aedcfaf4b1e5cfe54c4cee8ea4f2
Shital2000 <sdhengle231@gmail.com>
2022-05-30 22:06:36 +0530

    initial

---
+++ demomvc1/src/main/java/com/example/demomvc1/Student.java
@@ -0,0 +1,54 @@
+package com.example.demomvc1;
+
+public class Student {
+private String firstname;
+private String lastname;
+private String email;
+private String gender;
+private String plang;
+private String os;
+private String city;
+public String getFirstname() {
+	return firstname;
+}
+public void setFirstname(String firstname) {
+	this.firstname = firstname;
+}
+public String getLastname() {
+	return lastname;
+}
+public void setLastname(String lastname) {
+	this.lastname = lastname;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getGender() {
+	return gender;
+}
+public void setGender(String gender) {
+	this.gender = gender;
+}
+public String getPlang() {
+	return plang;
+}
+public void setPlang(String plang) {
+	this.plang = plang;
+}
+public String getOs() {
+	return os;
+}
+public void setOs(String os) {
+	this.os = os;
+}
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+}
+
