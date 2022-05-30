71214b2d21ef36ddde727048e2a303147bf86667
Shital2000 <sdhengle231@gmail.com>
2022-05-30 21:50:47 +0530

    initial

---
+++ defaultsecurity1/src/main/java/com/example/defaultsecurity1/Defaultsecurity1Application.java
@@ -0,0 +1,13 @@
+package com.example.defaultsecurity1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Defaultsecurity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Defaultsecurity1Application.class, args);
+	}
+
+}
---
+++ defaultsecurity1/src/test/java/com/example/defaultsecurity1/Defaultsecurity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.defaultsecurity1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Defaultsecurity1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
