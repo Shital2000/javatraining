4cb2621881fff622faecf3274c3d37119ee67089
Shital2000 <sdhengle231@gmail.com>
2022-05-31 12:32:03 +0530

    initial

---
+++ session2-Assignment/src/test/java/com/example/session2/Assignment/Session2AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.session2.Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Session2AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
